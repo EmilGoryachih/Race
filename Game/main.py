b135ee728d8abe7cbaa0d0dd2e70e4d0531c531b
Emil <emil12.05.2005@gmail.com>
2022-01-11 15:39:11 +0300

    version 0.1 (menu)

---
+++ Game/main.py
@@ -0,0 +1,73 @@
+import pygame
+import random, sys, os, time
+from pygame.locals import *
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', 'images', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+
+    return image
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ["ЗАСТАВКА", "",
+                  "Правила игры",
+                  "Если в правилах несколько строк,",
+                  "приходится выводить их построчно"]
+    fon = pygame.transform.scale(load_image('fon.png'), (1200, 675))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+if __name__ == '__main__':
+    # Изображение не получится загрузить
+    # без предварительной инициализации pygame
+
+    pygame.init()
+    size = width, height = 1200, 675
+    screen = pygame.display.set_mode(size)
+
+    FPS = 50
+    clock = pygame.time.Clock()
+
+    start_screen()
+
+    running = True
+    while running:
+        screen.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                terminate()
+        pygame.display.flip()
+        clock.tick(FPS)
+    pygame.quit()
